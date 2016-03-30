7a72786ceeabfc8cc08a346357e5b34655b944df
VengefulMollusc <ennead_coil@hotmail.com>
2016-03-30 23:17:47 +1300

    Updated changes from easter weekend
    reformatted pages, text search correctly working, Xquery search also
    working. splitting results according to file. showing both formatted and
    raw XML for each file

--- movieWebsite/routes/index.js
+++ movieWebsite/routes/index.js
@@ -15,9 +15,71 @@ router.get("/", function(req, res){
 	function (error, result){
 		if (error){console.error(error);}
 		else {
-			res.render('index', {title: 'ECS Video Rental', place: result.result});
+			res.render('index', {title: 'Colenso Project', place: result.result});
 		}
 	});
 });
+
+router.get('/search', function(req, res) {
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+	req.query.searchString, 
+	function (error, result){
+		if (error){console.error(error);}
+		else {
+			var results = result.result;
+			var resultArray = [];
+			var last = 0;
+			for (var i = 0, len = results.length; i < len; i++){
+				if (results[i] == "/"){
+					if (results.substring(i+1, i+5) == "TEI>"){
+						var singleResult = results.substring(last, i+5);
+						resultArray.push(singleResult);
+						last = i+5;
+					}
+				}
+			}
+			if (resultArray.length < 1){
+				resultArray.push(results);
+			}
+			res.render('search', { title: 'Colenso XQuery Search Results', content: req.query.searchString, 
+			searchResult: result.result, results: resultArray});
+		}
+	});
+});
+
+router.get('/textsearch', function(req, res) {
+	client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+	"ft:mark(db:open('Colenso')[. contains text '" + 
+	req.query.textSearchString
+	+"'])", 
+	function (error, result){
+		if (error){console.error(error);}
+		else {
+			var results = result.result;
+			var resultArray = [];
+			var last = 0;
+			for (var i = 0, len = results.length; i < len; i++){
+				if (results[i] == "/"){
+					if (results.substring(i+1, i+5) == "TEI>"){
+						var singleResult = results.substring(last, i+5);
+						resultArray.push(singleResult);
+						last = i+5;
+					}
+				}
+			}
+			if (resultArray.length < 1){
+				resultArray.push(results);
+			}
+			if (req.query.textSearchString == null){
+				res.render('textsearch', { title: 'Colenso Text Search Results', content: req.query.textSearchString, 
+				searchResult: "", results: ""});
+			} else {
+				res.render('textsearch', { title: 'Colenso Text Search Results', content: req.query.textSearchString, 
+				searchResult: result.result, results: resultArray});
+			}
+		}
+	});
+});
+
 
 module.exports = router;
